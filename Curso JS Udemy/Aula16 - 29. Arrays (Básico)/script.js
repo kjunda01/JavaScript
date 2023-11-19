598c39400bf0f59cf6506e9ebfd896f1d31ce765
kjunda01 <sinvaljuniorlms@gmail.com>
2023-11-19 17:28:55 -0300

    commit

--- Curso JS Udemy/Aula16 - 29. Arrays (Básico)/script.js
+++ Curso JS Udemy/Aula16 - 29. Arrays (Básico)/script.js
@@ -1,1 +1,25 @@
 // 29. Arrays (Básico)
+
+//            012345678910
+const nome = 'Sinval Junior';
+
+//               0       1        2
+const alunos = ['Luiz', 'Maria', 'João', ['1', '2', '3', '4'], true, 1, NaN, undefined, Infinity];
+
+console.log(alunos[3]);
+
+console.log(nome[0]);
+console.log(nome.charAt(0));
+
+console.log(alunos.length);
+
+alunos[alunos.length] = 'Giovana';
+console.log(alunos);
+alunos.push('Tiulinha');
+console.log(alunos);
+alunos.pop();
+console.log(alunos);
+alunos.unshift('Dulce');
+console.log(alunos);
+alunos.shift();
+console.log(alunos);
